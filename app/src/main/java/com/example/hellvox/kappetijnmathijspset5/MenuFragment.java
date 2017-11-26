2163d0f657e5c08f37ed1d90841c20381ee41c31
hellvox <33220067+hellvox@users.noreply.github.com>
2017-11-26 23:49:13 +0100

    Cleaning

--- app/src/main/java/com/example/hellvox/kappetijnmathijspset5/MenuFragment.java
+++ app/src/main/java/com/example/hellvox/kappetijnmathijspset5/MenuFragment.java
@@ -47,6 +47,19 @@ public class MenuFragment extends ListFragment {
         Bundle arguments = this.getArguments();
         menu_value = arguments.getString("category");
         menuItems = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, dishesArray);
+        getshizzle();
+        this.setListAdapter(menuItems);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Object object = this.getListAdapter().getItem(position);
+        String pen = object.toString();
+        Toast.makeText(getActivity().getApplicationContext(), pen, Toast.LENGTH_SHORT).show();
+    }
+
+    public void getshizzle() {
         String url = "https://resto.mprog.nl/menu";
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -68,16 +81,6 @@ public class MenuFragment extends ListFragment {
                         Toast.makeText(getActivity().getApplicationContext(), "Something went wrong, try again", Toast.LENGTH_SHORT).show();
                     }
                 });
-        // Access the RequestQueue through your singleton class.
         MySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(jsObjRequest);
-        this.setListAdapter(menuItems);
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Object object = this.getListAdapter().getItem(position);
-        String pen = object.toString();
-        Toast.makeText(getActivity().getApplicationContext(), pen, Toast.LENGTH_SHORT).show();
     }
 }
